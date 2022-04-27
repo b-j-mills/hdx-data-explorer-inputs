ffe5f0fca264a2649e2e88eca031ab815f164cfd
Briar <briar.j.mills@gmail.com>
2022-04-27 13:57:46 -0400

    Add download to pop raster script

--- scrapers/population_raster.py
+++ scrapers/population_raster.py
@@ -1,52 +1,54 @@
 import qgis.core
 import processing
-from os import listdir
-from os.path import join
+from os import mkdir
+from os.path import join, exists
+from urllib.request import urlretrieve
 
 
-def main():
+def main(countries):
+
     home_dir = QgsProject.instance().homePath()
+    temp_folder = join(home_dir, "pop_density")
+    if not exists(temp_folder):
+        mkdir(temp_folder)
 
-    for mapper in ["covid", "covid-arabic"]:
-        new_folder = join(home_dir, "Geoprocessing", mapper, "new", "pop_density")
-        latest_folder = join(home_dir, "Geoprocessing", mapper, "latest", "pop_density")
+    for iso in countries:
+        print(f"Processing population for {iso}")
 
-        r_list = [r for r in listdir(new_folder)]
-        for r in r_list:
+        ftp_url = f"ftp://ftp.worldpop.org.uk/GIS/Population/Global_2000_2020/2020/{iso.upper()}/{iso.lower()}_ppp_2020.tif"
+        r_file = join("temp_folder", f"{iso.lower()}_ppp_2020.tif")
+        try:
+            urlretrieve(ftp_url, r_file)
+        except:
+            print("Could not download file")
 
-            print(f"Processing {r} layer for mapper {mapper}")
+        r_layer = iface.addRasterLayer(r_file)
+        r_layer.loadNamedStyle(join("../config", "yellow-orange-red style.qml"))
 
-            r_file = join(new_folder, r)
+        # render
+        extent = r_layer.extent()
+        width = 3000
+        height = int(round(r_layer.height() / r_layer.width() * 3000, 0))
+        renderer = r_layer.renderer()
+        provider = r_layer.dataProvider()
 
-            r_layer = iface.addRasterLayer(r_file, r.split(".")[0])
+        pipe = QgsRasterPipe()
+        pipe.set(provider.clone())
+        pipe.set(renderer.clone())
 
-            r_layer.loadNamedStyle(join("../config", "yellow-orange-red style.qml"))
+        file_name = join(temp_folder, f"{iso.lower()}_ppp_2020_rendered.tif")
+        file_writer = QgsRasterFileWriter(file_name)
 
-            # render
-            extent = r_layer.extent()
-            width = 3000
-            height = int(round(r_layer.height() / r_layer.width() * 3000, 0))
-            renderer = r_layer.renderer()
-            provider=r_layer.dataProvider()
+        file_writer.writeRaster(pipe,
+                                width,
+                                height,
+                                extent,
+                                r_layer.crs())
 
-            pipe = QgsRasterPipe()
-            pipe.set(provider.clone())
-            pipe.set(renderer.clone())
+        QgsProject.instance().removeMapLayer(r_layer)
 
-            file_name = join(latest_folder, r)
-            file_writer = QgsRasterFileWriter(file_name)
-
-            file_writer.writeRaster(pipe,
-                                    width,
-                                    height,
-                                    extent,
-                                    r_layer.crs())
-
-            QgsProject.instance().removeMapLayer(r_layer)
-
-            print(f"Finished processing {r} layer")
+        print(f"Finished processing {iso}")
 
 
 if __name__ == "__main__":
-    main()
-
+    main([])  # fill in country list here
